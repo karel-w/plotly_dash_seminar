e5b6986529c5264cdb40b07946aae75dcd2b1297
karel-w <kjvanderweg@gmail.com>
2023-07-06 16:20:58 +0200

    added figure small cleanup

--- src/pages/99_empty_page.py
+++ src/pages/99_empty_page.py
@@ -1,5 +1,6 @@
 import dash
 from dash import html, dcc, callback, Input, Output
+import dash_mantine_components as dmc
 
 dash.register_page(
     __name__,
@@ -10,7 +11,29 @@ dash.register_page(
     )
 
 layout = html.Div(children=[
-    html.H1(children='enter your page here.'),
+    html.H1(children='This is a code example of a empty page.'),
+
+    dmc.Prism(
+        language='python',
+        withLineNumbers=True,
+        children='''
+import dash
+from dash import html, dcc, callback, Input, Output
+
+dash.register_page(
+    __name__,
+    path='/empty',
+    title='empty page',
+    name='empty page',
+    icon='fa-solid fa-file-circle-plus'
+    )
+
+layout = html.Div(children=[
+    html.H1(children='This is a code example of a empty page.'),
+    
+], style={'margin-left':'10px'})
+
+'''),
     
 ], style={'margin-left':'10px'})
 
